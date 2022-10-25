84f890dc5c219823edf40aba976b90e10b67df75
Mahdi Hasan <mahdihasan038@gmail.com>
2022-10-25 20:10:48 +0600

    creating pdf download file has ended

--- src/component/detailCard/DetailsCard.js
+++ src/component/detailCard/DetailsCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 
 import { FaCalendarAlt} from "react-icons/fa";
@@ -6,15 +6,24 @@ import { Link } from 'react-router-dom';
 import { FaMinus } from "react-icons/fa";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import { FaGem } from "react-icons/fa";
+import { useReactToPrint } from 'react-to-print';
 
-const DetailsCard = ({topics}) => {
+
+
+const DetailsCard = ({ topics }) => {
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  
+  });
+
   
   const { img, details, time,rating,Teacher, title,about, _id } = topics;
   
 
   return (
     <div className=''>
-     < h1 className='py-4'> <span className="mb-5 text-4xl font-bold"> Download Our Courses details <br /> in PDF </span>      <button className='btn bg-pink-400'><FaCloudDownloadAlt className='mx-2'></FaCloudDownloadAlt>Download</button>
+     < h1 className='py-4'> <span className="mb-5 text-4xl font-bold"> Download Our Courses details <br /> in PDF </span>      <button onClick={handlePrint} className='btn bg-pink-400'><FaCloudDownloadAlt className='mx-2'></FaCloudDownloadAlt>Download</button>
 </h1>
       <h1 className='text-center text-[#dc3545]'>        <FaMinus className='text-center  text-2xl  mx-auto'></FaMinus></h1>
 
@@ -22,7 +31,7 @@ const DetailsCard = ({topics}) => {
       <div>
     <div className="card card-compact w-full sm:w-[70%] lg:w-[60%] mx-auto mt-6 py-6  bg-base-100 shadow-xl">
 <figure><img className='h-[300px] w-full' src={img} alt="Shoes" /></figure>
-<div className="card-body ">
+<div ref={componentRef} className="card-body">
             <h2 className="card-title text-[#373737] font-bold capitalize">{title}</h2>
             <h2 className="card-title text-[#373737] font-bold">Teacher : {Teacher}</h2>
 
